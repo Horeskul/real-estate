a6588653c0c5f87cbd9f51247ee1330c912794bd
Ivan Horeskul <ivanhoreskul@gmail.com>
2023-02-18 15:24:03 +0200

    Added 3 sections with styles and functionality

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,24 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 6;
+let boxes = document.querySelectorAll('.featured__item');
+
+console.log(boxes);
+
+function loadMore() {
+    for (let i = currentItem; i < currentItem + 3; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    
+    currentItem += 3;
+
+    if(currentItem >= boxes.length) {
+        loadMoreBtn.style.display = 'none';
+    }
+}
+
+loadMoreBtn.addEventListener('click', loadMore);
